1d40dcceb682f55eb9ab0b07bf3ca37d09bae8b2
NosovK <nosov@nodeart.io>
2024-09-10 14:52:32 +0200

    add postback log

--- src/routes/api/list/[id]/+server.ts
+++ src/routes/api/list/[id]/+server.ts
@@ -1,6 +1,6 @@
 import { json, type RequestEvent } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import { getKV } from '$lib/utils/kv';
+import { getKV } from '$lib/kv';
 
 export const GET: RequestHandler = async ({ platform, params }: RequestEvent) => {
 	if (!platform) return json({ error: 'Platform is required' });
