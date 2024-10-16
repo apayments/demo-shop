5360f55ac36e5f71d64087fef1aa18fe2dd35c19
Daniyar <dstealord@gmail.com>
2024-10-17 03:56:10 +0500

    rm success load func

--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -1,11 +1,1 @@
-<script lang="ts">
-	export let data;
-
-	const { status, amount, currency, paymentId } = data;
-</script>
-
 <h1>SUCCESSFUL PAYMENT!</h1>
-<p>Status: {status}</p>
-<p>Amount: {amount} {currency}</p>
-<p>Payment ID: {paymentId}</p>
-<a href="/postbacks">See all postbacks</a>
